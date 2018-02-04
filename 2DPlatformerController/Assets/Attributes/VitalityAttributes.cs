395894ea6e724eb322ade67e7a3bf5f41257025b
mheloky@gmail.com <mheloky@gmail.com>
2018-02-03 22:28:43 -0500

    VitalityAttribute
    VitalityAttribute created (will porbably move team out of it and into something else into the future)

---
+++ 2DPlatformerController/Assets/Attributes/VitalityAttributes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Attributes
+{
+    public class VitalityAttributes
+    {
+        public string Team;
+        public int HP;
+        public bool IsInvincible;
+    }
+}
