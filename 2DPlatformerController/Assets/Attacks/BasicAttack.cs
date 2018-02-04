0e8d6a33104f1fe5fd7c61bc2c4a5db2685187de
mheloky@gmail.com <mheloky@gmail.com>
2018-02-03 22:07:18 -0500

    Added Basick Attack Class
    The attack mechanics have moved from each individual characters into attack classes.

---
+++ 2DPlatformerController/Assets/Attacks/BasicAttack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Abilities
+{
+    public class BasicAttack : IAttack
+    {
+        private List<IDamagable> targets = new List<IDamagable>();
+        private DamagableAttributes damagableAttributes;
+        /*1+20smth what if we have tier system?
+         * 2+20hp
+         * 3+20dmg
+         * 4+20armor
+         * */
+        public DamagableAttributes GetDamagableAttributes()
+        {
+            return damagableAttributes;
+        }
+
+        public IDamagable[] GetTargets()
+        {
+            return targets.ToArray();
+        }
+    }
+}
