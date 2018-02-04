ec9b22253e6c9fd953a59e9e9f8e8ff2a2bdc208
mheloky@gmail.com <mheloky@gmail.com>
2018-02-03 22:15:15 -0500

    IAttack- Attack Interface
    All attacks will be based on the IAttack interface. Note: abilities and attacks are exactly the same thing but we just call everything attacks

---
+++ 2DPlatformerController/Assets/Attacks/IAttack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Abilities
+{
+    public interface IAttack
+    {
+        DamagableAttributes GetDamagableAttributes();
+        IDamagable[] GetTargets();
+
+    }
+}
