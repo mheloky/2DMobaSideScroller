454c22300f2d1e92f762d24e6a1f2f345eef387e
mheloky <mheloky@gmail.com>
2018-12-29 22:11:15 -0500

    All
    All

--- 2dPlatformerEngine1/Assets/Assets/UI/Screens/IndividualGameRoomScreen/IndividualGameRoomScreen.cs
+++ 2dPlatformerEngine1/Assets/Assets/UI/Screens/IndividualGameRoomScreen/IndividualGameRoomScreen.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameRoomScreen : MonoBehaviour {
+public class IndividualGameRoomScreen : MonoBehaviour {
 
     public UIPresenter theUIPresenter = new UIPresenter();
     public bool isVisible = false;
