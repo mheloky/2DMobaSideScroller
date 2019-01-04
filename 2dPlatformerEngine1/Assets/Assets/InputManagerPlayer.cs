27234e24415fc2a8665ae63b5fe096dde2a481d2
mheloky <mheloky@gmail.com>
2019-01-03 22:21:08 -0500

    All
    All

--- 2dPlatformerEngine1/Assets/Assets/InputManagerPlayer.cs
+++ 2dPlatformerEngine1/Assets/Assets/InputManagerPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using Assets._2DPlatformer.Scripts.BaseEngine.GameStructure;
 using System.Collections;
 using System.Collections.Generic;
+using TCPIPGame.Messages;
 using UnityEngine;
 
 public class InputManagerPlayer : MonoBehaviour
@@ -8,17 +9,33 @@ public class InputManagerPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameRoomStatus.TheNetworkManager.OnSendUserInputResponseReceived += TheNetworkManager_OnSendUserInputResponseReceived;
+    }
+
+    public void ExecuteInput(int clientID, float horizontalAxis, bool jump)
+    {
+        Vector2 move = Vector2.zero;
+        move.x = horizontalAxis;
+
+        var playerBody = GameRoomStatus.GetPhysicalPlayer(clientID);
+        playerBody.NetworkHorizontalAxis = move.x;
+        playerBody.NetworkJump = jump;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = Vector2.zero;
-        move.x = Input.GetAxis("Horizontal");
+        var horizontalAxis= Input.GetAxis("Horizontal");
         var jump = Input.GetButtonDown("Jump");
-        var playerBody = GameRoomStatus.GetPhysicalPlayer(GameRoomStatus.ClientID);
-        playerBody.NetworkHorizontalAxis= move.x;
-        playerBody.NetworkJump = jump;
+        ExecuteInput(GameRoomStatus.ClientID, horizontalAxis, jump);
+        GameRoomStatus.TheNetworkManager.SendMessageToServer(new MessageSendUserInputRequest(new UserInput(horizontalAxis, jump)));
+    }
+
+    private void TheNetworkManager_OnSendUserInputResponseReceived(object sender, MessageSendUserInputResponse e)
+    {
+        if(e.ClientID!=GameRoomStatus.ClientID)
+        {
+            ExecuteInput(e.ClientID, e.TheUserInput.HorizontalAxis, e.TheUserInput.Jump);
+        }
     }
 }
