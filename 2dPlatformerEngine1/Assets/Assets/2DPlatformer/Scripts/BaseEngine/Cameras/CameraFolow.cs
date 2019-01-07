770cb2167bc497788aae4fb05a61ed1c05fe36c1
mheloky <mheloky@gmail.com>
2019-01-06 22:43:51 -0500

    All
    All

---
+++ 2dPlatformerEngine1/Assets/Assets/2DPlatformer/Scripts/BaseEngine/Cameras/CameraFolow.cs
@@ -0,0 +1,35 @@
+﻿using Assets._2DPlatformer.Scripts.BaseEngine.GameStructure;
+using Assets._2DPlatformer.Scripts.BaseEngine.Level;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFolow : MonoBehaviour
+{
+    Player TheMainPlayer;
+    public GrassPlainsLevel TheLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //this.GetComponent<Camera>().orthographicSize = 15;
+       TheMainPlayer = GameRoomStatus.GetThisMainPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //- 1.630475
+
+        if (TheMainPlayer.transform.position.x > TheLevel.GetLeftCameraBoundry().transform.position.x &&
+            TheMainPlayer.transform.position.x < TheLevel.GetRightCameraBoundry().transform.position.x)
+        {
+            this.transform.position = new Vector3(TheMainPlayer.transform.position.x,
+                this.transform.position.y, this.transform.position.z);
+        }
+        else
+        {
+            Debug.Log("Wont move camera");
+        }
+    }
+}
---
+++ 2dPlatformerEngine1/Assets/Assets/GrassPlainsLevel.cs
@@ -0,0 +1,32 @@
+﻿using Assets._2DPlatformer.Scripts.BaseEngine.Level;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassPlainsLevel : MonoBehaviour,ALevel
+{
+    public GameObject LeftCameraBoundry;
+    public GameObject RightCameraBoundry;
+
+    public GameObject GetLeftCameraBoundry()
+    {
+        return LeftCameraBoundry;
+    }
+
+    public GameObject GetRightCameraBoundry()
+    {
+        return RightCameraBoundry;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
